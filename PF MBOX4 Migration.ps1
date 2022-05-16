a8e8cd7b75e6ce1d1610d1533df87db9740bd5f0
puseyr <pusey.ronald@gmail.com>
2022-05-15 21:50:25 -0400

    Adding PS1 Script

---
+++ PF MBOX4 Migration.ps1
@@ -0,0 +1,13 @@
+﻿
+$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://exchange.server.net/PowerShell/ -Authentication Kerberos -Credential $UserCredential
+Import-PSSession $Session
+
+[void][Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
+
+$title = 'User E-Mail'
+$msg   = 'Enter user E-Mail:'
+
+$email = [Microsoft.VisualBasic.Interaction]::InputBox($msg, $title)
+
+set-mailbox -identity $email -defaultPublicFolderMailbox “CATORDAG001-PF-MBX04"
